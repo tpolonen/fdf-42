2ff01e452b459b130e1691aef3947f806fa38e67
tpolonen <tpolonen@student.hive.fi>
2022-01-20 22:42:30 +0200

    new test file

--- tests/helloworld.c
+++ tests/helloworld.c
@@ -1,11 +1,66 @@
 #include <mlx.h>
+#include <stddef.h>
+#include <stdio.h>
+#define MAX_DRAW 64
+
+typedef struct s_param
+{
+	void	*win;
+	void	*mlx;
+} t_param;
+
+
+static void	(*draw_funcs[MAX_DRAW]) (void *) = { 0 };
+
+int add_draw_func(void (*draw_func) (void *))
+{
+	int i = 0;
+	while (i < MAX_DRAW && draw_funcs[i] != NULL) {
+		i++;
+	} if (i == MAX_DRAW) return (-1);
+	draw_funcs[i] = draw_func;
+	printf("draw funcs now contains:\n");
+	for (int j = 0; j < MAX_DRAW && draw_funcs[j] != NULL; j++)
+		printf("* %p\n", &draw_funcs[j]);
+	return (1);
+}
+
+int update(void *params)
+{
+	printf("update running\n");
+	int i = 0;
+	while (i < MAX_DRAW && draw_funcs[i] != NULL)
+		draw_funcs[i++](params);
+	printf("draw funcs processed\n");
+	return (0);
+}
+
+void	draw_hello(void *params)
+{
+	static unsigned char r, g, b;
+	r += 2;
+	g += 4;
+	b += 8;
+	int color = b;
+	color |= (g << (sizeof(char) * 8));
+	color |= (r << ((sizeof(char) * 8) * 2));
+	mlx_string_put(((t_param *)params)->mlx, ((t_param *)params)->win, 320, 180, color, "Hello world!");
+	printf("color is %d\n", color);
+}
 
 int main(void)
 {
 	void	*mlx;
 	void	*win;
+	t_param	params;
 
 	mlx = mlx_init();
 	win = mlx_new_window(mlx, 640, 360, "Hwello uWu");
+	params.mlx = mlx;
+	params.win = win;
+	
+	printf("add_draw returned %d\n", add_draw_func(&draw_hello));
+	mlx_expose_hook(win, update, &params);
 	mlx_loop(mlx);
+	return (0);
 }
---
+++ tests/tutorial1.c
@@ -0,0 +1,112 @@
+#include <mlx.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#define SIZE_X 640
+#define SIZE_Y 360
+typedef struct s_image
+{
+	void	*img;
+	void	*addr;
+	int		bits_per_pixel;
+	int		bytes_per_line;
+	int		endian;
+} t_image;
+
+typedef struct s_param
+{
+	void	*win;
+	void	*mlx;
+	t_image	*bufs[2];
+} t_param;
+int	fill_color(t_image *img, int color)
+{
+	uint8_t	*dst;
+	int		i;
+
+	dst = (uint8_t *)img->addr;
+	i = 0;
+	while (i < (SIZE_X * SIZE_Y)) {
+		*(uint32_t *)dst = color;
+		dst += img->bits_per_pixel / 8;
+		i++;
+	}
+	return (0);
+}
+
+int	alternate_bands(t_image *img, uint32_t color, int bands)
+{
+	static int	offset;
+	uint8_t 	*dst;
+	int			x;
+	int			y;
+	int			height;
+	int			draw;
+
+	dst = (uint8_t *)img->addr;
+	height = SIZE_Y / bands;
+	y = offset;
+	draw = bands % 2;
+	while (bands > 0) {
+		if (bands % 2 == draw) {
+			int start_y = y;
+			while (y < start_y + height && y < SIZE_Y) {
+				x = 0;
+				while (x < SIZE_X) {
+					*(uint32_t *)(dst + (y * img->bytes_per_line) + x * (img->bits_per_pixel / 8)) = color;
+					x++;
+				}
+				y++;
+			}
+			bands--;
+		} else {
+			int start_y = y;
+			while (y < start_y + height && y < SIZE_Y) y++;
+			bands--;
+		}
+	}
+	offset++;
+	if (offset > height) offset = 0;
+	return (0);
+}
+
+int	render_frame(void *params)
+{
+	static int	cur_buf;
+	t_param		*p;
+	t_image		*buf;
+
+	p = (t_param *)params;
+	buf = (t_image *)p->bufs[cur_buf];
+	fill_color(buf, 0x00BA42);
+	alternate_bands(buf, 0x0042AACC, 6);
+	mlx_put_image_to_window(p->mlx, p->win, buf->img, 0, 0);
+	cur_buf = (cur_buf == 0);
+	return (0);
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*win;
+	t_param	params;
+	t_image	img1;
+	t_image img2;
+
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, SIZE_X, SIZE_Y, "tutorial1");
+	img1.img = mlx_new_image(mlx, SIZE_X, SIZE_Y);
+	img1.addr = mlx_get_data_addr(img1.img, &img1.bits_per_pixel, &img1.bytes_per_line, &img1.endian);
+	img2.img = mlx_new_image(mlx, SIZE_X, SIZE_Y);
+	img2.addr = mlx_get_data_addr(img2.img, &img2.bits_per_pixel, &img2.bytes_per_line, &img2.endian);
+	printf("bits per pixel: %d, line length: %d, endian: %d\n", img1.bits_per_pixel, img1.bytes_per_line, img1.endian);
+	params.mlx = mlx;
+	params.win = win;
+	params.bufs[0] = &img1;
+	params.bufs[1] = &img2;
+	render_frame(&params);
+	mlx_loop_hook(mlx, &render_frame, &params);
+	mlx_loop(mlx);
+	return (0);
+}
