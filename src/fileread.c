94883a7103775f5925ba64965b9b03fe5f7cf0c5
tpolonen <tpolonen@student.hive.fi>
2022-04-06 16:16:20 +0300

    more work on fileread

--- src/fileread.c
+++ src/fileread.c
@@ -6,30 +6,47 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 12:02:54 by tpolonen          #+#    #+#             */
-/*   Updated: 2022/04/06 14:24:58 by tpolonen         ###   ########.fr       */
+/*   Updated: 2022/04/06 15:29:34 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include "dintarr.h"
 
-void read_file(char* filename, t_param *params)
+static int	read_cols(char *strtptr)
 {
-	int			fd;
-	int			rows;
-	int			i;
-	int			cols;
-	int			offset;
-	int			len;
-	int			**map;
-	t_dstr		*data;
-	t_dintarr	*row;
-	char		*buf;
+	(void) strtptr;
+	return (1);
+}
+
+static int	read_rows(int fd, t_dstr *data)
+{
+	int		rows;
+	int		len;
+	char	*buf;
+
+	rows = 0;
+	while (1)
+	{
+		len = ft_getline(fd, &buf);
+		if (len <= 0)
+			return (rows);
+		ft_dstrbuild(&data, buf, len);
+		ft_dstrbuild(&data, "\n", 1);
+		rows++;
+	}
+}
+
+void		read_file(char* filename, t_param *params)
+{
+	int		fd;
+	int		rows;
+	int		**map;
+	int		*cols;
+	t_dstr	*data;
 	
 	(void) params;
 	(void) cols;
-	(void) row;
-	(void) offset;
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
@@ -38,18 +55,11 @@ void read_file(char* filename, t_param *params)
 	}
 	rows = 0;
 	data = (t_dstr *)ft_memalloc(sizeof(t_dstr));
-	while(1)
-	{
-		len = ft_getline(fd, &buf);
-		if (len <= 0)
-			break;
-		ft_dstrbuild(&data, buf, len);
-		rows++;
-	}
+	rows = read_rows(fd, data);
 	map = (int **)ft_memalloc(sizeof(int *) * rows);
-	i = 0;
-	while(i < rows)
+	while (1)
 	{
-		i++;
+		read_cols(data->str);
+		break ;
 	}
 }
